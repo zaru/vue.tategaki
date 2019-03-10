9243fecba89f4bbc2648d8a7508d3a15dea7660c
zaru <zarutofu@gmail.com>
2019-03-10 14:37:50 +0900

    fix build config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "semi": ["error", "never", {"beforeStatementContinuationChars": "never"}]
   },
   parserOptions: {
     parser: "babel-eslint"
