a1302cb45cf32d09a4548c60083fdd1b1ed315a4
zaru <zarutofu@gmail.com>
2019-06-01 22:57:13 +0900

    fix: paste plain text

--- src/lib/copy_paste.js
+++ src/lib/copy_paste.js
@@ -19,40 +19,60 @@ const getRangeAndOffsetAtBottom = (range) => {
   }
 }
 
+const hasBreakLine = (text) => {
+  return text.includes('\n')
+}
+
+const getNode = (text) => {
+  if (hasBreakLine(text)) {
+    const cleanHTMLs = text.split('\n').map(line => {
+      return line ? `<p>${line}</p>` : `<p><br></p>`
+    })
+    const cleanHTML = cleanHTMLs.join('')
+    const range = document.createRange()
+    return range.createContextualFragment(cleanHTML)
+  }
+  return document.createTextNode(text)
+}
+
 // ペーストしたテキストの中から段組みに必要な情報以外は消して追加する
 export const paste = (e) => {
   const text = window.clipboardData
     ? window.clipboardData.getData('text')
     : e.clipboardData.getData('text/plain')
-  const cleanHTMLs = text.split("\n").map(line => {
-    return line ? `<p>${line}</p>` : `<p><br></p>`
-  })
-  const cleanHTML = cleanHTMLs.join('')
   const sel = window.getSelection()
   const range = sel.getRangeAt(0)
-  const node = range.createContextualFragment(cleanHTML)
+  const node = getNode(text)
   range.insertNode(node)
 
-  // MEMO: 文途中で insert すると後ろのテキストが TextNode になってしまうので
-  // <p> で新しく element を作って置き換えてあげる
-  range.commonAncestorContainer.childNodes.forEach(node => {
-    if (node.nodeType === 3 && node.textContent !== '') {
-      const element = document.createElement('p')
-      element.innerText = node.textContent
-      node.parentNode.replaceChild(element, node)
-    }
-  })
+  if (hasBreakLine(text)) {
+    // MEMO: 文途中で insert すると後ろのテキストが TextNode になってしまうので
+    // <p> で新しく element を作って置き換えてあげる
+    range.commonAncestorContainer.childNodes.forEach(node => {
+      if (node.nodeType === 3 && node.textContent !== '') {
+        const element = document.createElement('p')
+        element.innerText = node.textContent
+        node.parentNode.replaceChild(element, node)
+      }
+    })
 
-  const newRange = document.createRange()
-  // 文中でペーストしたときに caret の位置をペーストの最後の node にする
-  if (range.commonAncestorContainer.childNodes.length !== cleanHTMLs.length + 1) {
-    const { node, offset } = getRangeAndOffsetInTheText(range)
-    newRange.setStart(node, offset)
+    const newRange = document.createRange()
+    // 文中でペーストしたときに caret の位置をペーストの最後の node にする
+    if (range.commonAncestorContainer.childNodes.length !== text.split('\n').length + 1) {
+      const { node, offset } = getRangeAndOffsetInTheText(range)
+      newRange.setStart(node, offset)
+    } else {
+      const { node, offset } = getRangeAndOffsetAtBottom(range)
+      newRange.setStart(node, offset)
+    }
+    newRange.collapse(true)
+    sel.removeAllRanges()
+    sel.addRange(newRange)
   } else {
-    const { node, offset } = getRangeAndOffsetAtBottom(range)
-    newRange.setStart(node, offset)
+    range.setStartAfter(node)
+    range.setEndAfter(node)
+    sel.removeAllRanges()
+    sel.addRange(range)
   }
-  newRange.collapse(true)
-  sel.removeAllRanges()
-  sel.addRange(newRange)
+
 }
