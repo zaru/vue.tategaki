6c250649aa35150df2d90ec3c9e6e14e68ca2b55
zaru <zarutofu@gmail.com>
2019-06-02 00:25:16 +0900

    fix: demo page

---
+++ src/example/Mobile.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <vue-tategaki
+      class="container"
+      key="1"
+      content=""
+      @updated="update"
+      :styles="styles1"
+      :placeholder="`place holder`"
+    >
+    </vue-tategaki>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import VueTategaki from '../VueTategaki.vue'
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    VueTategaki
+  },
+  data() {
+    return {
+      styles1: {
+        container: {
+          minWidth: '100%',
+          height: '500px',
+          fontSize: '16px',
+        }
+      }
+    }
+  },
+  methods: {
+    update(e) {
+      console.log(e)
+    }
+  },
+  mounted() {
+  }
+})
+</script>
+
+<style scoped>
+  #app {
+    width: 100%;
+  }
+</style>
